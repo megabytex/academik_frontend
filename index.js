a3a446f59a6f90a2e3659eebd679d664fecfdd89
Jaider <jacontreras@utb.edu.co>
2023-08-18 19:34:51 -0500

    agregué el titulo de mi app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var nav_menu = {
     template : `
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Academik App</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
